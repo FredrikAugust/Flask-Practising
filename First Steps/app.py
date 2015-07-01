ecb076558c12bc14f2c19dd846ad8296d2e52018
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-02 01:33:41 +0200

    Gets the argument from the URL passed

--- First Steps/app.py
+++ First Steps/app.py
@@ -2,11 +2,13 @@
 __author__ = 'Fredrik A. Madsen-Malmo'
 
 from flask import Flask
+from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__)  # Magic stuff I'm not prepared to understand
 
-@app.route('/')
-def index():
-	return 'This is me testing Flask!'
+@app.route('/')  # This is a decorator, kinda like attributes in C#
+def index(name='Unassigned'):
+	name = request.args.get('name', name)  # This get's the name variable frmo the GET req.
+	return 'This is {} testing Flask!'.format(name)
 
-app.run(debug=True, port=8080, host='0.0.0.0')
+app.run(debug=True, port=8080, host='0.0.0.0')  # Debug updates when I make changes, neat
