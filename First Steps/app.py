577d78961f533ad997859585c57bc9b611e49941
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-02 01:50:28 +0200

    added concat and addition methods

--- First Steps/app.py
+++ First Steps/app.py
@@ -6,9 +6,23 @@ from flask import request
 
 app = Flask(__name__)  # Magic stuff I'm not prepared to understand
 
+# INDEX #
+
 @app.route('/')  # This is a decorator, kinda like attributes in C#
+@app.route('/<name>')  # You can now visit /fredrik to get same effect as GET-string
 def index(name='Unassigned'):
-	name = request.args.get('name', name)  # This get's the name variable frmo the GET req.
 	return 'This is {} testing Flask!'.format(name)
+
+# CONCAT PAGE #
+
+@app.route('/concat/<part1>/<part2>')
+def concat(part1, part2):
+	return 'If you concatenate {} and {}, you will get {}'.format(part1, part2, part1+part2)
+
+# ADD PAGE #
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+	return '{} + {} = {}'.format(num1, num2, num1 + num2)
 
 app.run(debug=True, port=8080, host='0.0.0.0')  # Debug updates when I make changes, neat
